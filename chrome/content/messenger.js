436a9702381338cb0f5ca86645ac281e59519ba6
Ryan Turner <zdbiohazard2@gmail.com>
2014-09-22 21:15:12 -0700

    Fall-back to parsing X-Spam-Status.

--- chrome/content/messenger.js
+++ chrome/content/messenger.js
@@ -1,6 +1,14 @@
 function getSpamScore ( aHdr ) {
     var score = parseFloat(aHdr.getStringProperty("x-spam-score"));
 
+    // Fall-back to parsing X-Spam-Status if X-Spam-Score isn't valid.
+    if ( isNaN(score) ){
+        var header = aHdr.getStringProperty("x-spam-status");
+        var match = header.match(/ score=(-?[0-9]+\.?[0-9]*) /);
+        var score = parseFloat(( match != null ) ? match[1] : "");
+    }
+
+    // Return nothing if we somehow have a non-number.
     if ( isNaN(score) )
         return "";
 
@@ -30,11 +38,14 @@ function doOnceLoaded ( ) {
     var observer_service = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
     observer_service.addObserver(CreateDBObserver, "MsgCreateDBView", false);
 
-    // Add the "X-Spam-Score" header to the DB list if it's not present.
+    // Add the headers we want to the DB list if they're not present.
     var user_prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-    var db_headers = user_prefs.getCharPref("mailnews.customDBHeaders");
-    if ( db_headers.split(" ").indexOf("x-spam-score") == -1 )
-        user_prefs.setCharPref("mailnews.customDBHeaders", db_headers + " x-spam-score");
+    var headers = ["x-spam-score", "x-spam-status"];
+    for ( var hdr = 0 ; hdr < headers.length ; hdr++ ) {
+        var db_headers = user_prefs.getCharPref("mailnews.customDBHeaders");
+        if ( db_headers.split(" ").indexOf(headers[hdr]) == -1 )
+            user_prefs.setCharPref("mailnews.customDBHeaders", (db_headers + " " + headers[hdr]).trim());
+    }
 };
 
 window.addEventListener("load", doOnceLoaded, false);
